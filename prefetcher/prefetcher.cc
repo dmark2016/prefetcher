d0c29d35ab170da958b6a73f7b0059dff7ecc2c3
Øyvind Robertsen <oyvind@arcticstyle.net>
2016-02-29 13:35:49 +0100

    table size 1024, yields equel performance as reference rpt implementation

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include "interface.hh"
 
-#define RPT_SIZE 512
+#define RPT_SIZE 1024
 #define INITIAL 0
 #define TRAINING 1
 #define PREFETCH 2
@@ -32,45 +32,42 @@ void prefetch_init(void)
 void prefetch_access(AccessStat stat)
 {
     Entry e;
-    //if (stat.miss) {
-        int index = stat.pc % RPT_SIZE;
-        if (rpt[index].pc == 0 or rpt[index].pc != stat.pc) {
-            // New entry
-            e = *((Entry*) malloc(sizeof(Entry)));
-            e.pc = stat.pc;
-            e.last = stat.mem_addr;
-            e.stride = 0;
-            e.state = INITIAL;
-            rpt[index] = e;
-        } else {
-            // Subsequent miss
-            e = rpt[index];
-            switch (e.state) {
-                case INITIAL:
-                    {
-                        e.stride = stat.mem_addr - e.last;
-                        e.state = TRAINING;
-                        e.last = stat.mem_addr;
-                        break;
+    int index = stat.pc % RPT_SIZE;
+    if (rpt[index].pc == 0 or rpt[index].pc != stat.pc) {
+        // New entry
+        e = *((Entry*) malloc(sizeof(Entry)));
+        e.pc = stat.pc;
+        e.last = stat.mem_addr;
+        e.stride = 0;
+        e.state = INITIAL;
+        rpt[index] = e;
+    } else {
+        // Subsequent miss
+        e = rpt[index];
+        switch (e.state) {
+            case INITIAL:
+                {
+                    e.stride = stat.mem_addr - e.last;
+                    e.state = TRAINING;
+                    e.last = stat.mem_addr;
+                    break;
+                }
+            case TRAINING:
+                {
+                    int delta = stat.mem_addr - e.last;
+                    if (delta == e.stride) {
+                        e.state = PREFETCH;
+                    } else {
+                        e.stride = delta;
                     }
-                case TRAINING:
-                    {
-                        int delta = stat.mem_addr - e.last;
-                        if (delta == e.stride) {
-                            e.state = PREFETCH;
-                        } else {
-                            e.stride = delta;
-                        }
-                    }
-            }
-            rpt[index] = e;
-    //    }
+                }
+        }
+        rpt[index] = e;
     }
     if (e.state == PREFETCH && !in_cache(stat.mem_addr + e.stride) && stat.mem_addr + e.stride < MAX_PHYS_MEM_ADDR) {
         issue_prefetch(stat.mem_addr + e.stride);
     }
-}
-
+} 
 void prefetch_complete(Addr addr) {
     /*
      * Called when a block requested by the prefetcher has been loaded.
