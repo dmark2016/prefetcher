b890ddb0605da3ae0d0cea9062b000f58238b2cb
Truls Rustad Fossum <trulfos@gmail.com>
2016-04-11 11:10:05 +0000

    Fix bug in sequential prefetcher

--- prefetcher/prefetcher.cc
+++ prefetcher/prefetcher.cc
@@ -18,23 +18,24 @@ void prefetch_init(void)
 void prefetch_access(AccessStat stat)
 {
     /* pf_addr is now an address within the _next_ cache block */
-    Addr pf_addr = stat.mem_addr + BLOCK_SIZE;
+    Addr pf_addr = stat.mem_addr;
 
     /*
      * Issue a prefetch request if a demand miss occured,
      * and the block is not already in cache.
      */
     if (stat.miss && !in_cache(pf_addr)) {
-        issue_prefetch(pf_addr);
-        issue_prefetch(pf_addr + BLOCK_SIZE);
-        issue_prefetch(pf_addr + 2 * BLOCK_SIZE);
-        issue_prefetch(pf_addr + 3 * BLOCK_SIZE);
-        issue_prefetch(pf_addr + 4 * BLOCK_SIZE);
+        for (int i = 0; i < DEGREE; i++) {
+            pf_addr += BLOCK_SIZE;
+            issue_prefetch(pf_addr);
+        }
     } else {
       if (!in_cache(pf_addr + 2 * BLOCK_SIZE)) {
-        issue_prefetch(pf_addr + 2 * BLOCK_SIZE);
-        issue_prefetch(pf_addr + 3 * BLOCK_SIZE);
-        issue_prefetch(pf_addr + 4 * BLOCK_SIZE);
+        pf_addr += 2*BLOCK_SIZE;
+        for (int i = 2; i < DEGREE; i++) {
+            pf_addr += BLOCK_SIZE;
+            issue_prefetch(pf_addr);
+        }
       }
     }
 }
